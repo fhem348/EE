2dce13445aebebb5aa40c8e4a21b5eb46d4f51d9
fhem348 <fhem348@gmail.com>
2024-02-06 11:39:07 +0900

    roter

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const userRouter = require('./routers/user.router')
+const resumeRouter = require('./routers/resume.router')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(bodyParser.json())
+
+app.use('/users', userRouter)
+app.use('/resumes', resumeRouter)
 
 app.listen(port, () => {
     console.log(`이 포트로 연결된데요! ${port}`)
