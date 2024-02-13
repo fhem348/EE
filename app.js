aa9e813c9bed859e3fb8e9d0bde6120606509bdf
gksaprk <gksaprk@gmail.com>
2024-02-13 17:31:48 +0900

    2번쨰

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const userRouter = require('./routers/user.router');
+const postRouter = require('./routers/post.router');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.use('/users', userRouter);
+app.use('/posts', postRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ routers/post.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('zzzzqqqqqq');
+});
+
+module.exports = router;
